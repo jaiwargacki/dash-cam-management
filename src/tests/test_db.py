b66d632961b13dbc0d4c2bf044df536ad295c752
Jai Wargacki <jpw5681@rit.edu>
2024-02-05 19:29:42 -0500

    First attempt at yml file for github actions

---
+++ src/tests/test_db.py
@@ -0,0 +1,11 @@
+""" Test database related elements of the project 
+@Author: Jai Wargacki"""
+
+import psycopg2
+
+def test_database_connection():
+    try:
+        conn = psycopg2.connect("dbname='dashcam' user='postgres' host='localhost' password='postgres'")
+        assert True
+    except:
+        assert False
