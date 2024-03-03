629fabca4d03598c8e5a27227788e3bb23a6819c
Jai Wargacki <jpw5681@rit.edu>
2024-03-03 16:36:53 -0500

    Fix small typo in sql init

--- src/tests/test_db.py
+++ src/tests/test_db.py
@@ -10,3 +10,12 @@ def test_database_connection():
     except Exception as e:
         print(e)
         assert False
+
+def test_database_number_of_tables():
+    conn = psycopg2.connect("dbname='dashcam' user='postgres' host='localhost' password='postgres'")
+    cur = conn.cursor()
+    cur.execute("SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'public';")
+    count = cur.fetchone()[0]
+    assert count == 11
+    cur.close()
+    conn.close()
