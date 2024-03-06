4e4bcf97e22577ee6cff386e2b8fc60dc71d71e0
Jai Wargacki <jpw5681@rit.edu>
2024-03-05 23:15:52 -0500

    Location and date extraction

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
 """ General functions for processing video data
 @author: Jai Wargacki"""
 
-import cv2, os
+import cv2, os, time, math
 from concurrent.futures import ThreadPoolExecutor
 
 def move_to_storage(file_path: str, storage_path: str, delete: bool=False) -> None:
@@ -24,45 +24,92 @@ def move_to_storage(file_path: str, storage_path: str, delete: bool=False) -> No
 
 class Feature: 
     """ A metadata feature of a video """
-    def __init__(self, name: str, description: str):
+    def __init__(self, name: str, description: str, frame_frequency: int=1):
         self.name = name
         self.description = description
+        self.frame_frequency = frame_frequency
 
     def __str__(self):
         return f"{self.name}: {self.description}"
 
-class Video:
-    """ A video file """
-    def __init__(self, file_path: str):
-        self.file_path = file_path
+    def clear(self) -> None:
+        """ Clear the feature data """
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def process(self, frame) -> None:
+        """ Process a frame of the video """
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def save(self, trip_id: int) -> None:
+        """ Save the feature data """
+        raise NotImplementedError("Subclasses must implement this method")
+
+class Processing:
+    """ A processing object """
+    def __init__(self, verbose: bool=False):
         self.features = []
+        self.verbose = verbose
+        self.video_path = None
 
     def add_feature(self, feature: Feature):
         self.features.append(feature)
 
-    def process(self, features: list[Feature], num_workers: int=7) -> None:
+    def video_info(self, video: cv2.VideoCapture) -> dict:
+        """ Get the information of the video """
+        fps = video.get(cv2.CAP_PROP_FPS)
+        frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        duration = frame_count / fps
+        return {
+            "fps": fps,
+            "frame_count": frame_count,
+            "duration": duration
+        }
+
+    def process(self, video_path: str) -> None:
         """ Process the video file using a list of features
-        :param features: A list of Feature objects to derive from the video
-        :param num_workers: The number of workers to use in parallel processing (default 7)
+        :param video_path: The path to the video file
         """
-        video_capture = cv2.VideoCapture(self.file_path)
-        with ThreadPoolExecutor(max_workers=num_workers) as executor:
+        # Clear the features
+        for feature in self.features:
+            feature.clear()
+        self.video_path = video_path
+        video_capture = cv2.VideoCapture(self.video_path)
+        video_info = self.video_info(video_capture)
+
+        if self.verbose:
+            print(f"Processing video at {self.video_path}")
+            print(f"Video Info: {video_info}")
+     
+        start_time = time.time()
+        with ThreadPoolExecutor() as executor:
+            frame_count = 0
             while True:
                 ret, frame = video_capture.read()
                 if not ret:
                     break
-                for feature in features:
-                    executor.submit(feature.process, frame)
+
+                for feature in self.features:
+                    if frame_count % feature.frame_frequency == 0:
+                        executor.submit(feature.process, frame)
+                frame_count += 1
             executor.shutdown(wait=True)
         video_capture.release()
 
-    def save(self, storage_path: str, delete: bool=False) -> None:
-        """ Save the video file to the storage location
-        :param storage_path: The path to move the file to
-        :param delete: Whether or not to delete the file after moving
-        """
-        move_to_storage(self.file_path, storage_path, delete)
+        if self.verbose:
+            print(f"Processing took {time.time() - start_time} seconds")
 
-    def __str__(self):
-        return f"Video: {self.file_path}"
+    def save(self) -> None:
+        """ Save the features """
+        if self.video_path is None:
+            raise ValueError("No video path was given")
+
+        if self.verbose:
+            print("Saving features")
+
+        # Create or get the trip id
+        trip_id = 1
+
+        # Save the features
+        for feature in self.features:
+            feature.save(trip_id)
 
---
+++ src/tests/test_feature_trip_data.py
@@ -0,0 +1,66 @@
+""" Test feature trip data module
+@Author: Jai Wargacki"""
+
+import cv2
+
+from feature_trip_data import TripData
+
+def test_trip_data_init():
+    feature = TripData()
+    assert feature.name == "TripData"
+    assert feature.description == "Extracts the position, time, and speed of the vehicle from the video"
+    assert feature.frame_frequency == 15
+    assert feature.data_points == []
+    assert feature.reader is not None
+
+def test_trip_data_clear():
+    feature = TripData()
+    feature.data_points = ["test"]
+    feature.clear()
+    assert feature.data_points == []
+
+def _test_trip_data_process(path, expected_time, expected_lat, expected_lon):
+    feature = TripData()
+    frame = cv2.imread(path)
+    feature.process(frame)
+
+    assert len(feature.data_points) == 1
+    actual = feature.data_points[0]
+    assert actual["time"] == expected_time
+    assert actual["lat"] == expected_lat
+    assert actual["lon"] == expected_lon
+
+def test_trip_data_process_1():
+    path = "tests/test_data/frame01.jpg"
+    expected_time = "01/23/2024 11:43:06"
+    expected_lat = "N43.093457"
+    expected_lon = "W77.649113"
+    _test_trip_data_process(path, expected_time, expected_lat, expected_lon)
+
+def test_trip_data_process_2():
+    path = "tests/test_data/frame02.jpg"
+    expected_time = "01/23/2024 11:44:12"
+    expected_lat = "N43.093270"
+    expected_lon = "W77.659285"
+    _test_trip_data_process(path, expected_time, expected_lat, expected_lon)
+
+def test_trip_data_process_3():
+    path = "tests/test_data/frame03.jpg"
+    expected_time = "01/27/2024 21:01:46"
+    expected_lat = "N43.087085"
+    expected_lon = "W77.593351"
+    _test_trip_data_process(path, expected_time, expected_lat, expected_lon)
+
+def test_trip_data_process_4():
+    path = "tests/test_data/frame04.jpg"
+    expected_time = "01/31/2024 11:49:43"
+    expected_lat = "N43.088721"
+    expected_lon = "W77.677018"
+    _test_trip_data_process(path, expected_time, expected_lat, expected_lon)
+
+def test_trip_data_process_5():
+    path = "tests/test_data/frame05.jpg"
+    expected_time = "01/31/2024 17:52:51"
+    expected_lat = "N43.086873"
+    expected_lon = "W77.679858"
+    _test_trip_data_process(path, expected_time, expected_lat, expected_lon)
